40e30085287ce31ca0194f2b1ff1e45af56c899a
IT21201196 <99582425+IT21201196@users.noreply.github.com>
2022-02-16 13:15:40 +0000

    What did you change

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+  int mark1,mark2,total=0;
+  float average;
+
+  printf("Enter mark1: ");
+  scanf("%d",&mark1);
+  printf("Enter mark2: ");
+  scanf("%d",&mark2);
+
+  total= mark1 + mark2;
+  average=total/2;
+
+  printf("average=%.2f",average);
+
   
   return 0;
 }
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,20 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  int distance;
+  float amount;
+
+  printf("Enter distance travelled: ");
+  scanf("%d",&distance);
+
+  if (distance<=30){
+    amount = distance*50;
+  }
+  else if(distance>30){
+    amount = ((amount-30)*40)+(30*50);
+  }
+  printf("Total amount= %.2f",amount);
+
   
   return 0;
 }
